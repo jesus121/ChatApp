dd443ce77fde81c8007de3a849c59a3656a8a3c0
jesus <jesusbuenoperales@gmail.com>
2018-06-07 12:16:05 +0200

    con el adapter

--- app/src/main/java/com/android/teaching/chatapp/ChatActivity.java
+++ app/src/main/java/com/android/teaching/chatapp/ChatActivity.java
@@ -1,12 +1,17 @@
 package com.android.teaching.chatapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -33,5 +38,30 @@ public class ChatActivity extends AppCompatActivity {
         Intent intent =new Intent(this , NewMessageActivity.class);
         startActivity(intent);
         return true;
+    }
+
+    private class Myadapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater =(LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View roview = inflater.inflate(R.layout.plantilla,parent,false);
+            return null;
+        }
     }
 }
